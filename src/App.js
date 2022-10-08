975f72d03ab5741a145ff3d96619cd2168612a47
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-10-09 00:05:17 +0530

    Added login form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+
 function App() {
     return (
         <div className="App">
             <Header />
-            <Hero />
-            <Services />
-            <Contact />
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <Hero />
+                                <Services />
+                                <Contact />
+                            </>
+                        }
+                    />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
